26bb74337c4856e139ab559ae913eb3ed357750d
Lars Knoke <l.knoke@colorplus.de>
2022-08-24 17:41:53 +0200

    build -> _site

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,25 @@
-const filters = require('./utils/filters.js')
-const transforms = require('./utils/transforms.js')
-const collections = require('./utils/collections.js')
+const filters = require("./utils/filters.js");
+const transforms = require("./utils/transforms.js");
+const collections = require("./utils/collections.js");
 
 module.exports = function (eleventyConfig) {
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static");
 
-	// Filters 
+	// Filters
 	Object.keys(filters).forEach((filterName) => {
-		eleventyConfig.addFilter(filterName, filters[filterName])
-	})
+		eleventyConfig.addFilter(filterName, filters[filterName]);
+	});
 
 	// Transforms
 	Object.keys(transforms).forEach((transformName) => {
-		eleventyConfig.addTransform(transformName, transforms[transformName])
-	})
+		eleventyConfig.addTransform(transformName, transforms[transformName]);
+	});
 
 	// Collections
 	Object.keys(collections).forEach((collectionName) => {
-		eleventyConfig.addCollection(collectionName, collections[collectionName])
-	})
+		eleventyConfig.addCollection(collectionName, collections[collectionName]);
+	});
 
 	// This allows Eleventy to watch for file changes during local development.
 	eleventyConfig.setUseGitIgnore(false);
@@ -27,14 +27,14 @@ module.exports = function (eleventyConfig) {
 	return {
 		dir: {
 			input: "src/",
-			output: "dist",
+			output: "_site",
 			includes: "_includes",
-			layouts: "_layouts"
+			layouts: "_layouts",
 		},
 		templateFormats: ["html", "md", "njk"],
 		htmlTemplateEngine: "njk",
 
 		// 1.1 Enable eleventy to pass dirs specified above
-		passthroughFileCopy: true
+		passthroughFileCopy: true,
 	};
 };
