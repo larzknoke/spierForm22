d8d8a66ebfe0a9cf27b15453305cdefc5a02a240
Lars Knoke <l.knoke@colorplus.de>
2022-08-26 07:15:54 +0200

    reCaptcha

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -4,41 +4,53 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	document
 		.querySelector(".spierForm")
 		.addEventListener("submit", function (event) {
-			var data = this;
-			fetch(data.getAttribute("action"), {
-				method: data.getAttribute("method"),
-				body: new FormData(data),
-			})
-				.then((res) => res.json())
-				.then(function (data) {
-					console.log("data", data.success);
-					if (data.success) {
-						document.querySelector(".spierForm").reset();
-						// document.querySelector(".msg-box").remove();
-						const button = document.querySelector(".submit-button");
-						const msgBox = document.createElement("div");
-						msgBox.classList.add(
-							"text-center",
-							"font-bold",
-							"text-xl",
-							"bg-spier-100",
-							"rounded-sm",
-							"text-white",
-							"py-3",
-							"px-6",
-							"mb-3",
-							"msg-box"
-						);
-						msgBox.textContent = "Formular erfolgreich versendet!";
-						button.after(msgBox);
-					} else {
-						alert("Es ist ein Fehler aufgetreten.");
-					}
-				})
-				.catch((e) => {
-					console.log(e);
-					return e;
-				});
 			event.preventDefault();
+			var data = this;
+
+			grecaptcha.ready(function () {
+				grecaptcha
+					.execute("6LfsiqshAAAAAJfWhN9qQlKKSbQIdizcSoO_jjPa", {
+						action: "submit",
+					})
+					.then(function (token) {
+						const formData = new FormData(data);
+						formData.append("token", token);
+
+						fetch(data.getAttribute("action"), {
+							method: data.getAttribute("method"),
+							body: formData,
+						})
+							.then((res) => res.json())
+							.then(function (data) {
+								console.log("data", data.success);
+								if (data.success) {
+									document.querySelector(".spierForm").reset();
+									// document.querySelector(".msg-box").remove();
+									const button = document.querySelector(".submit-button");
+									const msgBox = document.createElement("div");
+									msgBox.classList.add(
+										"text-center",
+										"font-bold",
+										"text-xl",
+										"bg-spier-100",
+										"rounded-sm",
+										"text-white",
+										"py-3",
+										"px-6",
+										"mb-3",
+										"msg-box"
+									);
+									msgBox.textContent = "Formular erfolgreich versendet!";
+									button.after(msgBox);
+								} else {
+									alert("Es ist ein Fehler aufgetreten.");
+								}
+							})
+							.catch((e) => {
+								console.log(e);
+								return e;
+							});
+					});
+			});
 		});
 });
